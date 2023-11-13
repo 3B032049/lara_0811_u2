1890261e6bb91c7d875a415403e80168cababc70
鄭哲丞 <a0968518713@gmail.com>
2023-11-13 21:44:22 +0800

    設定 Route 跳轉路由

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-
+/*
 Route::get('/', function () {
     return 'welcome';
 });
+*/
 
+
+Route::get('r2', function() {
+    return view('welcome');
+});
+Route::get('r1', function() {
+    return redirect('r2');
+});
