ce4850a727cc9268a2f7c8b47a1cc48db4a0a640
鄭哲丞 <a0968518713@gmail.com>
2023-11-13 22:04:03 +0800

    設定 Route 前置

--- routes/web.php
+++ routes/web.php
@@ -23,20 +23,31 @@ Route::get('/', function () {
 });
 */
 
-
+/*
 Route::get('r2', function() {
     return view('welcome');
 });
 Route::get('r1', function() {
     return redirect('r2');
 });
-
-
+*/
+/*
 Route::get('hello/{name}',function($name){
     return'Hello,'.$name;
 });
-
+*/
+/*
 Route::get('hello/{name?}', function($name='Everybody') {
     return 'Hello, '.$name;
 })->name('hello.index');
+*/
 
+Route::get('dashboard', function() {
+    return 'dashboard';
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('dashboard', function() {
+        return 'admin dashboard';
+    });
+});
